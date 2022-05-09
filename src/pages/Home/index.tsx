9190cd9c02d6d0a9bd61b57f8daa1f37a3d740f3
Jonatan Timoteo <jonatan_ts@hotmail.com>
2022-05-09 12:45:28 -0300

    Home finish

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,11 +54,11 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      {cart.map(product => (
+      {products.map(product => (
         <li key={product.id}>
           <img src={product.image} alt={product.title}/>
           <strong>{product.title}</strong>
-          <span>{product.price}</span>
+          <span>{product.priceFormatted}</span>
           <button
             type="button"
             data-testid="add-product-button"
@@ -66,7 +66,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {cartItemsAmount[product.id] || 0} 2
+            {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
