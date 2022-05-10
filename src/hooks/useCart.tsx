7f89677c7765ec7bb70c89420f8975efe3e91694
Jonatan Timoteo <jonatan_ts@hotmail.com>
2022-05-10 10:03:21 -0300

    cart working

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -72,9 +72,20 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updateCart = [...cart]
+      const productIndex = updateCart.findIndex(product => product.id === productId)
+
+      if (productIndex >= 0) {
+        updateCart.splice(productIndex, 1)
+        setCart(updateCart)
+
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart))
+      } else {
+        throw Error()
+      }
+
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
